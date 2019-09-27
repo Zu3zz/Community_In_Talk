95d97e4f45f4096bb27f791be5a8306fa35a140e
Zu3zz <zthzththth@163.com>
2019-09-27 18:18:57 +0800

    init repo

---
+++ src/main/java/com/zth/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.zth.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Author: 3zZ.
+ * Date: 2019/9/27 6:05 下午
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
